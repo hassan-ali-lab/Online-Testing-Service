cee2c6ee58417dba4ae709a45010b25edbe42ac8
Ramsha <ramsha.bscsf19@iba-suk.edu.pk>
2022-08-12 07:05:40 +0500

    More Questions Added

--- src/data.js
+++ src/data.js
@@ -1,9 +1,123 @@
 export const Data = [
     {
-      id: 0,
-      question: ` What is the Capital Of pakistan ?`,
-      options: [`karachi`, `lahore`, `islambad`],
-      answer: `islambad`
+        id: 0,
+        question: `Any part of the graphic that is not included in another hot zone is considered to be part of `,
+        options: [`Rect`, `Point`, `Default`],
+        answer: `Default`
+    },
+    {
+        id: 1,
+      question: ` Which web app attribute is defined by the statement:”A large number of users may access the WebApp at one time”?`,
+      options: [`Performance`, ` Concurrency`, ` Network intensiveness`],
+      answer: `Concurrency`
+    },
+    {
+        id: 2,
+        question: `Which web app attribute is defined by the statement:”The quality and aesthetic nature of content remains an important determinant of the quality of a WebApp”? `,
+        options: [`Availability`, `Continuous evolution`, ` Content sensitive`],
+        answer: ` Content sensitive`
+    },
+    {
+        id: 3,
+        question: ` If the user queries a collection of large databases and extracts information from the webapp, the webapp is categorized under `,
+        options: [`Database access app`, `Service oriented app`, ` Data warehousing app`],
+        answer: `Data warehousing app`
+    },
+    {
+        id: 4,
+        question: `Which process model should be used in virtually all situations of web engineering? `,
+        options: [`Incremental Model`, `spiral Model`, `water fall Model`],
+        answer: `Incremental Model`
+    },
+    {
+        id: 5,
+        question: ` Which analysis is a part of Analysis model of the web engineering process framework? `,
+        options: [`Interaction Analysis`, ` Functional Analysis`, `Both`],
+        answer: `Both`
+    },
+    {
+        id: 6,
+        question: `Web development and software development are one and the same thing. `,
+        options: [`True`, `False`, `Both`],
+        answer: `False`
+    },
+    {
+        id: 7,
+        question: `Which of the following statements are incorrect with reference to web-based systems? Web-based systems `,
+        options: [`should be unscalable`, ` are subject to assorted legal, social, and ethical scrutiny`, `must be able to cope with uncertain, random heavy demands on services`],
+        answer: `should be unscalable`
+    },
+    {
+        id: 8,
+        question: ` What category of web-based system would you assign to electronic shopping?`,
+        options: [` Workflow-oriented`, `Transaction-oriented`, ` Collaborative work`],
+        answer: `Transaction-oriented`
+    },
+    {
+        id: 9,
+        question: `What category of web-based system would you assign to discussion groups? `,
+        options: [`Online communities`, `Collaborative work`, ` Web portals`],
+        answer: `Online communities`
+    },
+    {
+        id: 10,
+        question: ` W3C stands for `,
+        options: [` World Wide Web Community`, `World Wide Web Collaboration`, `World Wide Web Consortium`],
+        answer: `World Wide Web Consortium`
+    },
+    {
+        id: 11,
+        question: `Which of the following is a risk associated with using hypertext in web applications? `,
+        options: [`Loss of sense of locality and direction`, `Cognitive overload for users`, `Both`],
+        answer: `Both`
+    },
+    {
+        id: 12,
+        question: `  From which tag the descriptive list starts?`,
+        options: [` <LL>`, `<DD>`, `<DL>`],
+        answer: `<DL>`
+    },
+    {
+        id: 13,
+        question: `The elements and have the following characteristics `,
+        options: [`Elements and define content to be inline or block-level`, ` Element inherits properties defined for in a stylesheet`, `have no real meanings as html tags unless stylesheet is applied`],
+        answer: `Elements and define content to be inline or block-level`
+    },
+    {
+        id: 14,
+        question: `The latest HTML standard is`,
+        options: [` XML`, `HTML 5.0`, ` HTML 4.0`],
+        answer: `HTML 5.0`
+    },
+    {
+        id: 15,
+        question: `. While working on a JavaScript project, in your JavaScript application, which function would you use to send messages to users requesting for text input? `,
+        options: [` Prompt()`, `GetInput()`, `console.log()`],
+        answer: ` Prompt()`
+    },
+    {
+        id: 16,
+        question: `The function returns a Boolean value that indicates whether a specified expression can be evaluated as a number; in ASP is `,
+        options: [` IsNotNumeric()`, `IsNumber()`, ` IsNumeric()`],
+        answer: ` IsNumeric()`
+    },
+    {
+        id: 17,
+        question: `HTML is a subset of`,
+        options: [`SGML`, `SGMT`, `SGMXL`],
+        answer: `SGML`
+    }
+    ,{
+        id: 18,
+        question: `Which of the following tag is used to mark a beginning of paragraph? `,
+        options: [`</p>`, `<p>`, `<param>`],
+        answer: `<p>`
+    },
+    {
+        id: 19,
+        question: ` The body tag usually used after`,
+        options: [`EM tag`, ` Title tag`, ` HEAD tag`],
+        answer: ` HEAD tag`
     }
   ];
   
